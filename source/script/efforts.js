e5f5e4d8c6cf5f7abef2cdd44b3f595870eb25bc
Andei Khabarov <akhabarow@gmail.com>
2021-01-19 16:58:35 +0300

    Add the possibility to choose a custom period for requesting stats data.

--- source/script/efforts.js
+++ source/script/efforts.js
@@ -1,172 +1,264 @@
 (function () {
-    // Create the connector object
-    var myConnector = tableau.makeConnector();
+  // Create the connector object
+  var myConnector = tableau.makeConnector();
 
-    // Define the schema
-    myConnector.getSchema = function (schemaCallback) {
-        var cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.int
-        }, {
-            id: "month",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "name",
-            alias: "name",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "employeeName",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "effortForPeriod",
-            alias: "effortForPeriod",
-            dataType: tableau.dataTypeEnum.int
-        }, {
-            id: "officeId",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "employeeLogin",
-            dataType: tableau.dataTypeEnum.string
-        }];
+  // Define the schema
+  myConnector.getSchema = function (schemaCallback) {
+      var cols = [{
+              id: "id",
+              dataType: tableau.dataTypeEnum.int,
+          },
+          {
+              id: "month",
+              dataType: tableau.dataTypeEnum.string,
+          },
+          {
+              id: "name",
+              alias: "name",
+              dataType: tableau.dataTypeEnum.string,
+          },
+          {
+              id: "employeeName",
+              dataType: tableau.dataTypeEnum.string,
+          },
+          {
+              id: "effortForPeriod",
+              alias: "effortForPeriod",
+              dataType: tableau.dataTypeEnum.int,
+          },
+          {
+              id: "officeId",
+              dataType: tableau.dataTypeEnum.string,
+          },
+          {
+              id: "employeeLogin",
+              dataType: tableau.dataTypeEnum.string,
+          },
+      ];
 
-        var tableSchema = {
-            id: "efforts",
-            alias: "TTT Efforts",
-            columns: cols
-        };
+      var tableSchema = {
+          id: "TTT_Efforts",
+          alias: "TTT Efforts",
+          columns: cols
+      };
 
-        schemaCallback([tableSchema]);
-    };
+      schemaCallback([tableSchema]);
+  };
 
-    var API_KEY = "07286f74-5fff-49ae-9b23-c2b200770e94";
-    var BASE_URL = "https://ttt-dev-new.noveogroup.com/api/ttt/v1/statistic/projects/employees?";
+  var API_KEY = "07286f74-5fff-49ae-9b23-c2b200770e94";
+  var BASE_URL =
+      "https://ttt-dev-new.noveogroup.com/api/ttt/v1/statistic/projects/employees?";
 
-    var createURL = function (startDate, endDate) {
-        return BASE_URL + "startDate=" + startDate + "&" + "endDate=" + endDate;
-    }
+  var PERIODS = [];
 
-    // Download the data
-    myConnector.getData = function (table, doneCallback) {
-        $.ajaxSetup({
-            headers: {
-                'API_SECRET_TOKEN': API_KEY,
-                accept: 'application/json',
-            }
-        });
+  /**
+   * Создает URL адрес для получения статистики за текущий промежуток.
+   * @param {string} startDate 
+   * @param {string} endDate 
+   */
+  var createURL = function (startDate, endDate) {
+      return BASE_URL + "startDate=" + startDate + "&" + "endDate=" + endDate;
+  };
 
-        var PERIODS = [{
-            id: 1,
-            startDate: '01-01',
-            endDate: '01-31',
-        }, {
-            id: 2,
-            startDate: '02-01',
-            endDate: '02-29',
-        }, {
-            id: 3,
-            startDate: '03-01',
-            endDate: '03-31',
-        }, {
-            id: 4,
-            startDate: '04-01',
-            endDate: '04-30',
-        }, {
-            id: 5,
-            startDate: '05-01',
-            endDate: '05-31',
-        }, {
-            id: 6,
-            startDate: '06-01',
-            endDate: '06-30',
-        }, {
-            id: 7,
-            startDate: '07-01',
-            endDate: '07-31',
-        }, {
-            id: 8,
-            startDate: '08-01',
-            endDate: '08-31',
-        }, {
-            id: 9,
-            startDate: '09-01',
-            endDate: '09-30',
-        }, {
-            id: 10,
-            startDate: '10-01',
-            endDate: '10-31',
-        }, {
-            id: 11,
-            startDate: '11-01',
-            endDate: '11-30',
-        }, {
-            id: 12,
-            startDate: '12-01',
-            endDate: '12-31',
-        }];
+  /**
+   * Вычисляет последний день в месяце. Умеет работать с високосным годом.
+   * @param {string} date 
+   */
+  var calcLastDay = (date) => {
+      var currentDate = new Date(date);
+      var lastDay = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth() + 1,
+          0
+      );
+      return lastDay.getDate();
+  };
 
-        var YEAR = '2020';
-        var tableData = [];
-        var transformJSON = function (resp, monthId, startDate) {
-            var pages = resp;
-            var tableDataForCurrentMonth = [];
+  /**
+   * Создает шаблонный объект для создания запроса на сервер.
+   * Применяются особые условия для первого и последнего запросов из периода т.к. пользователь не обязательно
+   * будет запрашивать данные с начала по конец месяца.
+   * @param {number} index 
+   * @param {number} maxIndex 
+   * @param {string} firstDate - данные из настроек пользователя. Дата начала сбора статистики.
+   * @param {string} lastDate - данные из настроек пользователя. Дата конца сбора статистики.
+   */
+  var createPeriodRequestObject = (index, maxIndex, firstDate, lastDate) => {
+      var date = new Date(firstDate);
+      var startMonth = date.getMonth();
+      var startYear = date.getFullYear();
+      var newDate = new Date(startYear, startMonth + index, 1);
 
-            // Iterate over the JSON object
-            for (var i = 0, len = pages.length; i < len; i++) {
-                var currentPage = pages[i];
-                var employees = currentPage.childNodeList;
+      var year = newDate.getFullYear();
 
-                for (var employee = 0, employeesTotal = employees.length; employee < employeesTotal; employee++) {
-                    tableDataForCurrentMonth.push({
-                        "id": currentPage.id,
-                        "month": `${YEAR}-${startDate}`,
-                        "name": currentPage.name,
-                        "employeeName": employees[employee].name,
-                        "effortForPeriod": employees[employee].effortForPeriod,
-                        "officeId": employees[employee].officeId,
-                        "employeeLogin": employees[employee].login,
-                    });
-                }
-            }
-            tableData[monthId - 1] = tableDataForCurrentMonth;
-            console.log('Data was received', monthId);
-        };
+      // Если номер месяца меньше 10, то нужно приписать "0" перед ним.
+      var month = newDate.getMonth() + 1 >= 9 ? newDate.getMonth() + 1 : `0${newDate.getMonth() + 1}`;
 
-        var createTable = function (tableData) {
-            tableData.forEach(function (dataByMonthArray) {
-                table.appendRows(dataByMonthArray);
-            });
-        };
+      // Обрабатываем кейс если день в startDate не будет началом месяца.
+      if (index === 0) {
+          return {
+              id: index + 1,
+              startDate: firstDate,
+              endDate: `${year}-${month}-${calcLastDay(newDate)}`,
+          };
+      }
 
-        // Save all requests in an array of jqXHR objects
-        var requests = PERIODS.map(function (dateObject) {
-            var {
-                id,
-                startDate,
-                endDate
-            } = dateObject;
+      // Обрабатываем кейс если день в endDate не будет концом месяца.
+      if (index === maxIndex) {
+          return {
+              id: index + 1,
+              startDate: `${year}-${month}-01`,
+              endDate: lastDate,
+          };
+      }
 
-            return $.ajax({
-                method: 'GET',
-                url: createURL(`${YEAR}-${startDate}`, `${YEAR}-${endDate}`),
-                success: (data) => transformJSON(data, id, startDate),
-            });
-        });
+      return {
+          id: index + 1,
+          startDate: `${year}-${month}-01`,
+          endDate: `${year}-${month}-${calcLastDay(newDate)}`,
+      };
+  };
 
-        $.when.apply(this, requests).then(function () {
-            // Each argument is an array with the following structure: [ data, statusText, jqXHR ]
-            console.log('Finished');
-            createTable(tableData);
-            doneCallback();
-        });
-    };
+  /**
+   * Вычисляет количество месячных периодов между startDate и endDate.
+   * Создает массив объектов, которые будут использованы для создания запросов на сервер.
+   * @param {string} startDate 
+   * @param {string} endDate
+   * @returns {
+   *  id: number,
+   *  startDate: string,
+   *  endDate: string,
+   * }[],
+   */
+  var createDatesArray = (startDate, endDate) => {
+      var datesArray = [];
+      var start = new Date(startDate);
+      var end = new Date(endDate);
+      var basicMonthDiff = (end.getFullYear() - start.getFullYear()) * 12 + 1;
+      basicMonthDiff -= start.getMonth();
+      basicMonthDiff += end.getMonth();
+      for (var index = 0; index < basicMonthDiff; index++) {
+          datesArray.push(
+              createPeriodRequestObject(index, basicMonthDiff - 1, startDate, endDate)
+          );
+      }
 
-    tableau.registerConnector(myConnector);
+      return datesArray;
+  };
 
-    // Create event listeners for when the user submits the form
-    $(document).ready(function () {
-        $("#submitButton").click(function () {
-            tableau.connectionName = "TTT Efforts"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
-        });
-    });
+  // Download the data
+  myConnector.getData = function (table, doneCallback) {
+      var { startDate, endDate, isSingleRadioChecked } = JSON.parse(tableau.connectionData);
+      console.log(startDate, endDate, isSingleRadioChecked);
+
+      // Конфигурируем заголовки для AJAX- запросов.
+      $.ajaxSetup({
+          headers: {
+              API_SECRET_TOKEN: API_KEY,
+              accept: "application/json",
+          },
+      });
+
+      var tableData = [];
+
+      /**
+       * Создает из полученного ответа массив со строками для таблицы
+       * @param {object} resp - ответ от сервера
+       * @param {number} requestId - порядковый номер запроса в массиве PERIODS
+       * @param {*} startDate - дата с которой начался период за который получаем статистику. Используется в таблице.
+       */
+      var transformJSON = function (resp, requestId, startDate) {
+          var pages = resp;
+          var tableDataForCurrentMonth = [];
+
+          // Iterate over the JSON object
+          for (var i = 0, len = pages.length; i < len; i++) {
+              var currentPage = pages[i];
+              var employees = currentPage.childNodeList;
+
+              for (
+                  var employee = 0, employeesTotal = employees.length; employee < employeesTotal; employee++
+              ) {
+                  tableDataForCurrentMonth.push({
+                      id: currentPage.id,
+                      month: startDate,
+                      name: currentPage.name,
+                      employeeName: employees[employee].name,
+                      effortForPeriod: employees[employee].effortForPeriod,
+                      officeId: employees[employee].officeId,
+                      employeeLogin: employees[employee].login,
+                  });
+              }
+          }
+          tableData[requestId - 1] = tableDataForCurrentMonth;
+      };
+
+      if (startDate !== "" && endDate !== "") {
+          if (isSingleRadioChecked) {
+              PERIODS.push({
+                  startDate,
+                  endDate,
+                  id: 1,
+              });
+          } else {
+              PERIODS = createDatesArray(startDate, endDate);
+          }
+      }
+
+      var createTable = function (tableData) {
+          tableData.forEach(function (dataByMonthArray) {
+              table.appendRows(dataByMonthArray);
+          });
+      };
+
+      // Save all requests in an array of jqXHR objects
+      var requests = PERIODS.map(function (dateObject) {
+          var {
+              id,
+              startDate,
+              endDate
+          } = dateObject;
+
+          console.log(id, startDate, endDate);
+
+          return $.ajax({
+              method: "GET",
+              url: createURL(startDate, endDate),
+              success: (data) => transformJSON(data, id, startDate),
+          });
+      });
+
+      $.when.apply(this, requests).then(function () {
+          // Each argument is an array with the following structure: [ data, statusText, jqXHR ]
+          console.log("Finished");
+          createTable(tableData);
+          doneCallback();
+      });
+  };
+
+  tableau.registerConnector(myConnector);
+
+  // Create event listeners for when the user submits the form
+  $(document).ready(function () {
+      $("#submitButton").click(function () {
+          var dateObj = {
+              startDate: $('#startDate').val().trim(),
+              endDate: $('#endDate').val().trim(),
+              isSingleRadioChecked: $('#singlePeriod').is(':checked'),
+          };
+  
+          function isValidDate(dateStr) {
+              var d = new Date(dateStr);
+              return !isNaN(d.getDate());
+          }
+  
+          if (isValidDate(dateObj.startDate) && isValidDate(dateObj.endDate)) {
+              tableau.connectionData = JSON.stringify(dateObj);
+              tableau.connectionName = "TTT Efforts";
+              tableau.submit();
+          } else {
+              $('#errorMsg').html("Enter valid dates. For example, 2016-05-08.");
+          }
+      });
+  });
 })();
